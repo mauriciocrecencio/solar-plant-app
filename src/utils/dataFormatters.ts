32200f428f16c9e119f404493f928d0fa0985bf4
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2023-06-18 23:54:29 -0300

    format data formatters functions

--- src/utils/dataFormatters.ts
+++ src/utils/dataFormatters.ts
@@ -1,14 +1,26 @@
-export function formatGeneration(generation: string) {
+export function formatGeneration(generation: string | number) {
   return new Intl.NumberFormat('pt-BR').format(Math.round(Number(generation)));
 }
 
 export function formatHour(hour: string) {
-  const parsedHour = parseInt(hour);
-  return parsedHour <= 12 ? `${parsedHour}AM` : `${parsedHour}PM`;
+  return parseInt(hour.split(':')[0]);
 }
 
 export function formatMonth(date: string) {
-  const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+  const months = [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Maio',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ];
   return months[parseInt(date.split('-')[1]) - 1];
 }
 
@@ -17,5 +29,5 @@ export function formatDay(date: string) {
 }
 
 export function formatYear(date: string) {
-  return new Intl.DateTimeFormat('pt-BR', { year: 'numeric' }).format(new Date(date));
+  return date.split('-')[0];
 }
